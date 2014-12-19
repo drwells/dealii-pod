1bbe4c47422685d624ca0cfa7b78b9ec86b611d4
David Wells <drwells@vt.edu>
2014-12-18 21:35:30 -0500

    More test work.

--- ns/test/nstest.cc
+++ ns/test/nstest.cc
@@ -1,17 +1,24 @@
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/tensor_base.h>
+
 #include <deal.II/dofs/dof_tools.h>
-#include <deal.II/grid/grid_generator.h>
+
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_values.h>
+
+#include <deal.II/grid/grid_generator.h>
+
+#include <deal.II/lac/block_vector.h>
+#include <deal.II/lac/vector.h>
 
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
 
-#include "ns.h"
+#include "../ns.h"
 
+using namespace dealii;
 namespace POD
 {
   namespace NavierStokes
@@ -20,9 +27,9 @@ namespace POD
     class Chebyshev
     {
     public:
-      Chebyshev(const dealii::Tensor<1, dim, int> od) : orders(od) {}
+      Chebyshev(const Tensor<1, dim, int> od) : orders(od) {}
 
-      double operator()(const dealii::Point<dim> point) const
+      double operator()(const Point<dim> point) const
       {
         double result = 1.0;
         for (unsigned int i = 0; i < dim; ++i)
@@ -31,26 +38,25 @@ namespace POD
           }
         return result;
       }
-
-    private:
-      dealii::Tensor<1, dim, int> orders;
+      
+      Tensor<1, dim, int> orders;
     };
 
 
     template<int dim>
-    void get_chebyshev_vector(const dealii::Tensor<1, dim, int> orders,
-                              const dealii::DoFHandler<dim> &dof_handler,
-                              const dealii::FEValues<dim>   &fe_values,
-                              dealii::Vector<double>        &coefficients)
+    void get_chebyshev_vector(const Tensor<1, dim, int> orders,
+                              const DoFHandler<dim> &dof_handler,
+                              const FEValues<dim>   &fe_values,
+                              Vector<double>        &coefficients)
     {
       coefficients.reinit(dof_handler.n_dofs());
-      auto& mapping = fe_values.get_mapping();
+      auto &mapping = fe_values.get_mapping();
 
       Chebyshev<dim> basis_function(orders);
-      std::vector<dealii::Point<dim>> support_points(dof_handler.n_dofs());
-      dealii::DoFTools::map_dofs_to_support_points(mapping,
-                                                   dof_handler,
-                                                   support_points);
+      std::vector<Point<dim>> support_points(dof_handler.n_dofs());
+      DoFTools::map_dofs_to_support_points(mapping,
+                                           dof_handler,
+                                           support_points);
       unsigned int i = 0;
       for (auto &point : support_points)
         {
@@ -61,46 +67,50 @@ namespace POD
   }
 }
 
-constexpr unsigned int problem_dim = 2;
+constexpr unsigned int dim = 2;
 
 using namespace POD::NavierStokes;
-int main(int argc, char** argv)
-  {
-    dealii::Triangulation<problem_dim> triangulation;
-    dealii::GridGenerator::hyper_cube (triangulation, -1, 1);
-    triangulation.refine_global (6);
-    dealii::DoFHandler<problem_dim> dof_handler (triangulation);
-    dealii::Tensor<1, problem_dim, int> orders;
-    dealii::FE_Q<problem_dim> fe (3); // TODO this crashes > 1
-    dealii::QGauss<problem_dim> quadrature (4);
-    dealii::FEValues<problem_dim> fe_values(fe, quadrature,
-                                    dealii::update_values
-                                    | dealii::update_gradients
-                                    | dealii::update_JxW_values);
+int main(int argc, char **argv)
+{
+  Triangulation<dim> triangulation;
+  GridGenerator::hyper_cube (triangulation, -1, 1);
+  triangulation.refine_global (6);
+  DoFHandler<dim> dof_handler (triangulation);
+  Tensor<1, dim, int> orders;
+  FE_Q<dim> fe (3); // TODO this crashes > 1
+  QGauss<dim> quadrature (6);
+  FEValues<dim> fe_values(fe, quadrature, update_values | update_gradients 
+  | update_JxW_values);
+  
+  dof_handler.distribute_dofs(fe);
+  std::cout << "DoFs: " << dof_handler.n_dofs() << std::endl;
 
-    dof_handler.distribute_dofs(fe);
-    std::cout << "DoFs: " << dof_handler.n_dofs() << std::endl;
+  unsigned int n_vectors = 3;
+  std::vector<BlockVector<double>> chebyshev_vectors;
+  for (unsigned int i = 0; i < n_vectors; ++i)
+    {
+      for (unsigned int j = 0; j < dim; ++j)
+        {
+          orders[j] = 3*j + 1 + 2*i;
+        }
+      BlockVector<double> chebyshev_vector(2);
+      get_chebyshev_vector(orders, dof_handler, fe_values, chebyshev_vector.block(0));
+      chebyshev_vector.block(1) = chebyshev_vector.block(0);
+      //chebyshev_vector.block(1) = 0.0;
+      chebyshev_vector.collect_sizes();
+      chebyshev_vectors.push_back(std::move(chebyshev_vector));
+      std::cout << "function order = " << orders << std::endl;
+    }
 
-    unsigned int n_vectors = 3;
-    std::vector<dealii::Vector<double>> chebyshev_vectors;
-    for (unsigned int i = 0; i < n_vectors; ++i)
-      {
-        for (unsigned int j = 0; j < problem_dim; ++j)
-          {
-            orders[j] = 3*j + 1;
-          }
-        dealii::Vector<double> chebyshev_vector;
-        get_chebyshev_vector(orders, dof_handler, fe_values, chebyshev_vector);
-        chebyshev_vectors.push_back(std::move(chebyshev_vector));
-      }
+  double result = trilinearity_term(fe,
+                                    quadrature,
+                                    dof_handler,
+                                    chebyshev_vectors[0],
+                                    chebyshev_vectors[1],
+                                    chebyshev_vectors[2]);
 
-    double result = compute_trilinearity(dof_handler, fe, fe_values, quadrature,
-                                         chebyshev_vectors[0],
-                                         chebyshev_vectors[1],
-                                         chebyshev_vectors[2], 0);
+  std::cout << "nonlinearity entry is " << result << std::endl;
 
-    std::cout << "nonlinearity entry is " << result << std::endl;
-
-    // to destroy the pointer from dof_handler to fe:
-    dof_handler.clear();
-  }
+  // to destroy the pointer from dof_handler to fe:
+  dof_handler.clear();
+}
--- ns/test/nstest.sage
+++ ns/test/nstest.sage
@@ -4,12 +4,20 @@
 import sage.all as sg
 
 def main():
-    x, y = sg.var('x, y')
-    a = sg.chebyshev_T(1, x)*sg.chebyshev_T(4, y)
-    b = sg.chebyshev_T(1, x)*sg.chebyshev_T(4, y)
-    c = sg.chebyshev_T(1, x)*sg.chebyshev_T(4, y)
+    x, y, z = sg.var('x, y, z')
+    orders = [[1, 4], [3, 6], [5, 8]]
+    a = sg.chebyshev_T(orders[0][0], x)*sg.chebyshev_T(orders[0][1], y)
+    a = [a, a, 0*x]
+    b = sg.chebyshev_T(orders[1][0], x)*sg.chebyshev_T(orders[1][1], y)
+    b = [b, b, 0*x]
+    c = sg.chebyshev_T(orders[2][0], x)*sg.chebyshev_T(orders[2][1], y)
+    c = [c, c, 0*x]
 
-    print sg.integrate(sg.integrate(a*b*c.diff(x), x, -1, 1), y, -1, 1).N(64)
+    # print sg.integrate(sg.integrate(a*b*c.diff(x), x, -1, 1), y, -1, 1).N(64)
+    convection = (a[0]*(b[0]*c[0].diff(x) + b[1]*c[0].diff(y) + b[2]*c[0].diff(z))
+                  + a[1]*(b[0]*c[1].diff(x) + b[1]*c[1].diff(y) + b[2]*c[1].diff(y))
+                  + a[2]*(b[0]*c[2].diff(x) + b[1]*c[2].diff(y) + b[2]*c[2].diff(y)))
+    print sg.integrate(sg.integrate(convection, -1, 1), -1, 1).N(64)
 
 if __name__ == '__main__':
     main()
